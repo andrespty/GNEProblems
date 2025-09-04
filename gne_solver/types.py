9b8771a248cf25b6358c7049e0c5a823696f23fe
Andres Ho <andresholee@gmail.com>
2025-09-04 14:34:06 -0400

    Changed typings for files in gne_solver

---
+++ gne_solver/types.py
@@ -0,0 +1,18 @@
+from typing import List, Tuple, Dict, Optional, Callable, Union, TypeAlias
+from numpy.typing import NDArray
+import numpy as np
+
+# Inputs
+Vector= NDArray[np.float64]
+Matrix = NDArray[np.float64]
+VectorList = List[Vector] # Player actions vectors
+
+# Functions
+ObjFunction = Callable[[VectorList], Vector]
+ObjFunctionGrad = Callable[[VectorList], Vector] # Can return a single float or a vector
+ConsFunction = Callable[[VectorList], Vector]
+ConsFunctionGrad = Callable[[VectorList], Vector] # Should always return either a scalar or a vector of same size as the number of actions
+
+PlayerConstraint = Union[List[int], None, List[None]]
+
+WrappedFunction = Callable[[List[float]], Vector]
